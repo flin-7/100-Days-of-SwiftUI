f9261fbd4a6d92ea531937201dac7ee5da030ff4
flin-7 <flin.7@icloud.com>
2019-10-25 23:33:03 -0700

    Day 33

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,93 +8,88 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var animationAmount2: CGFloat = 1
-    @State private var animationAmount = 0.0
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
     
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor)
+        .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
+struct ContentView: View {
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    @State private var isShowingRed = false
+    
+    let letters = Array("Hello SwiftUI")
     
     var body: some View {
-        VStack(spacing: 72) {
-            
-            Button("Tap Me") {
-                self.animationAmount2 += 1
-            }
-            .padding(50)
-            .background(Color.green)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount2)
-            .blur(radius: (animationAmount2 - 1) * 3)
-            .animation(.default)
-            
-            Button("Tap Me") {
-                //            self.animationAmount += 1
-                withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                    self.animationAmount += 360
-                }
-            }
-            .padding(50)
-            .background(Color.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 1, z: 1))
-            
-            Button("Tap Me") {
-                //                self.animationAmount2 += 1
-            }
-            .padding(50)
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .overlay(
-                Circle()
-                    .stroke(Color.blue)
-                    .scaleEffect(animationAmount2)
-                    .opacity(Double(2 - animationAmount2))
-                    .animation(
-                        Animation.easeOut(duration: 1)
-                            .repeatForever(autoreverses: false)
-                )
-            )
-                .onAppear {
-                    self.animationAmount2 = 2
-            }
-            
-        }
+        //        Button("Tap me") {
+        //            self.enabled.toggle()
+        //        }
+        //        .frame(width: 200, height: 200)
+        //        .background(enabled ? Color.blue : Color.red)
+        //        .animation(nil)
+        //        .foregroundColor(.white)
+        //        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+        //        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
         
-        //        .overlay(
-        //            Circle()
-        //                .stroke(Color.red)
-        //                .scaleEffect(animationAmount)
-        //                .opacity(Double(2 - animationAmount))
-        //                .animation(
-        //                    Animation.easeIn(duration: 1)
-        //                        .repeatForever(autoreverses: true)
-        //            )
-        //        )
-        //            .onAppear {
-        //                self.animationAmount = 2
-        //        }
-        //        print(animationAmount)
-        //
-        //        return VStack {
-        //
-        //            Stepper("Scale amount", value: $animationAmount.animation(
-        //                Animation.easeOut(duration: 1)
-        //                .repeatCount(3, autoreverses: true)
-        //            ), in: 1...10)
-        //
-        //            Spacer()
-        //
-        //            Button("Tap Me") {
-        //                self.animationAmount += 1
-        //            }
-        //            .padding(40)
-        //            .background(Color.red)
-        //            .foregroundColor(.white)
-        //            .clipShape(Circle())
-        //            .scaleEffect(animationAmount)
-        //        }
+//                LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomLeading)
+//                    .frame(width: 300, height: 200)
+//                    .clipShape(RoundedRectangle(cornerRadius: 10))
+//                    .offset(dragAmount)
+//                    .gesture(
+//                        DragGesture()
+//                            .onChanged { self.dragAmount = $0.translation }
+//                            .onEnded { _ in
+//                                withAnimation(.spring()) {
+//                                    self.dragAmount = .zero
+//                                }
+//                        }
+//                )
+        
+                HStack(spacing: 0) {
+                    ForEach(0..<letters.count) { num in
+                        Text(String(self.letters[num]))
+                            .padding(5)
+                            .font(.title)
+                            .background(self.enabled ? Color.blue : Color.red)
+                            .offset(self.dragAmount)
+                            .animation(Animation.default.delay(Double(num) / 20))
+                    }
+                }
+                .gesture(
+                    DragGesture()
+                        .onChanged { self.dragAmount = $0.translation }
+                        .onEnded { _ in
+                            self.dragAmount = .zero
+                            self.enabled.toggle()
+                    }
+                )
+        
+//        VStack {
+//            Button("Tap Me") {
+//                withAnimation {
+//                    self.isShowingRed.toggle()
+//                }
+//            }
+//
+//            if isShowingRed {
+//                Rectangle()
+//                .fill(Color.red)
+//                .frame(width: 200, height: 200)
+//                    .transition(.pivot)
+//            }
+//
+//        }
     }
 }
 
