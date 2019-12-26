03d8d01f3300b782acb46bdf18cef243972a5588
flin-7 <flin.7@icloud.com>
2019-12-26 15:18:29 -0800

    Day 50

---
+++ Cupcake Corner/Cupcake Corner/CheckoutView.swift
@@ -0,0 +1,41 @@
+//
+//  CheckoutView.swift
+//  Cupcake Corner
+//
+//  Created by Felix Lin on 12/26/19.
+//  Copyright © 2019 Felix Lin. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        GeometryReader { geo in
+            ScrollView {
+                VStack {
+                    Image("cupcakes")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width)
+                    
+                    Text("Your total is $\(self.order.cost, specifier: "%.2f")")
+                        .font(.title)
+                    
+                    Button("Place order") {
+                        
+                    }
+                    .padding()
+                }
+            }
+        }
+        .navigationBarTitle("Check out", displayMode: .inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
