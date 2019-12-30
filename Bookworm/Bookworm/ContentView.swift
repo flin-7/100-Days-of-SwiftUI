0cc9b573ebcc6eb627916481c137729c889e0bd4
flin-7 <flin.7@icloud.com>
2019-12-29 20:27:28 -0800

    Day 53

---
+++ Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Felix Lin on 12/29/19.
+//  Copyright © 2019 Felix Lin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
