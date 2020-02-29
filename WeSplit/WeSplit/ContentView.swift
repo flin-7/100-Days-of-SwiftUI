159b43833a2a1f228be7509c268fafea633796d5
flin-7 <flin.7@icloud.com>
2020-02-28 22:02:03 -0800

    DAY 16

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Felix Lin on 2/28/20.
+//  Copyright © 2020 Felix Lin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
