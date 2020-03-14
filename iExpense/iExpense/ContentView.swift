f95f2e1ba86cd023fb05d6bc7660c09fe8d43bce
flin-7 <flin.7@icloud.com>
2020-03-14 15:57:40 -0700

    Day 36

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Felix Lin on 3/14/20.
+//  Copyright © 2020 Felix Lin. All rights reserved.
+//
+
+import SwiftUI
+
+struct User: Codable {
+    var firstName: String
+    var lastName: String
+}
+
+struct ContentView: View {
+    @State private var user = User(firstName: "Taylor", lastName: "Swift")
+    
+    var body: some View {
+        Button("Save User") {
+            let encoder = JSONEncoder()
+            
+            if let data = try? encoder.encode(self.user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
