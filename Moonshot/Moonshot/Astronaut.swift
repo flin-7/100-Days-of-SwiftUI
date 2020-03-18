874f02679e729aeab02df8ca26b4cfe804cbe706
flin-7 <flin.7@icloud.com>
2020-03-17 17:19:31 -0700

    DAY 40

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Felix Lin on 3/17/20.
+//  Copyright © 2020 Felix Lin. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
