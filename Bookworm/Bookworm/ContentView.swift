25e17c2961b058d38e175bc2d921c18f4bff261f
flin-7 <flin.7@icloud.com>
2020-05-11 22:59:20 -0700

    DAY 53

---
+++ Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Felix Lin on 5/11/20.
+//  Copyright © 2020 Felix Lin. All rights reserved.
+//
+
+import SwiftUI
+
+//struct PushButton: View {
+//    let title: String
+//    @Binding var isOn: Bool
+//
+//    var onColors = [Color.red, Color.yellow]
+//    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+//
+//    var body: some View {
+//        Button(title) {
+//            self.isOn.toggle()
+//        }
+//        .padding()
+//        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
+//        .foregroundColor(.white)
+//        .clipShape(Capsule())
+//        .shadow(radius: isOn ? 0 : 5)
+//    }
+//}
+
+struct ContentView: View {
+    //    @State private var rememberMe = false
+    
+    //    @Environment(\.presentationMode) var presentationMode
+//    @Environment(\.horizontalSizeClass) var sizeClass
+    
+    @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
+    @Environment(\.managedObjectContext) var moc
+    
+    var body: some View {
+        VStack {
+            List {
+                ForEach(students, id: \.id) { student in
+                    Text(student.name ?? "Unknown")
+                }
+            }
+            
+            Button("Add") {
+                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+
+                let chosenFirstName = firstNames.randomElement()!
+                let chosenLastName = lastNames.randomElement()!
+                
+                let student = Student(context: self.moc)
+                student.id = UUID()
+                student.name = "\(chosenFirstName) \(chosenLastName)"
+                
+                try? self.moc.save()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
