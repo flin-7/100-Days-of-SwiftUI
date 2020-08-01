c0b444d4b77a99768a629da1358dd0f95b73983c
flin-7 <flin.7@icloud.com>
2020-08-01 15:44:35 -0700

    Project 14: Bucket List Overview

---
+++ BucketList/BucketList/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Felix Lin on 8/1/20.
+//  Copyright © 2020 Felix Lin. All rights reserved.
+//
+
+import SwiftUI
+import LocalAuthentication
+
+struct ContentView: View {
+    @State private var isUnlocked = false
+    
+    var body: some View {
+        VStack {
+            if self.isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { (success, authenticationError) in
+                DispatchQueue.main.async {
+                    if success {
+                        // authenticated successfully
+                        self.isUnlocked = true
+                    } else {
+                        // there was a problem
+                    }
+                }
+            }
+        } else {
+            // no biometrics
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
