0bbc558d06b77e9a99363d9cff5bc71876e9c941
flin-7 <flin.7@icloud.com>
2020-08-16 15:38:22 -0700

    Project 16: Hot Prospects

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Felix Lin on 8/14/20.
+//  Copyright © 2020 Felix Lin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var prospects = Prospects()
+    
+    var body: some View {
+        TabView {
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Image(systemName: "person.3")
+                    Text("Everyone")
+            }
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Image(systemName: "checkmark.circle")
+                    Text("Contacted")
+            }
+            ProspectsView(filter: .uncontacted)
+                .tabItem {
+                    Image(systemName: "questionmark.diamond")
+                    Text("Uncontacted")
+            }
+            MeView()
+                .tabItem {
+                    Image(systemName: "person.crop.square")
+                    Text("Me")
+            }
+        }
+        .environmentObject(prospects)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HotProspects/HotProspects/Prospect.swift
@@ -0,0 +1,50 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Felix Lin on 8/16/20.
+//  Copyright © 2020 Felix Lin. All rights reserved.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    let id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    fileprivate(set) var isContacted = false
+}
+
+class Prospects: ObservableObject {
+    @Published private(set) var people: [Prospect]
+    
+    static let saveKey = "SavedData"
+
+    init() {
+        if let data = UserDefaults.standard.data(forKey: Self.saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                self.people = decoded
+                return
+            }
+        }
+
+        self.people = []
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: Self.saveKey)
+        }
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
+    }
+    
+    func toggle(_ prospect: Prospect) {
+        objectWillChange.send()
+        prospect.isContacted.toggle()
+        save()
+    }
+}
